25312ca4e38ae33c5a8af8978ba567cb68f88ac4
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2021-04-26 21:41:05 -0300

    implement method findByCode for param long

--- src/main/java/br/com/algaworks/algamoney/api/repository/CategoriaRepository.java
+++ src/main/java/br/com/algaworks/algamoney/api/repository/CategoriaRepository.java
@@ -1,9 +1,12 @@
 package br.com.algaworks.algamoney.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.algaworks.algamoney.api.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
+	@Query(value = "select * from categoria where codigo = :codigo", nativeQuery = true)
+	public Categoria buscaPeloCodigo(Long codigo);
 }
